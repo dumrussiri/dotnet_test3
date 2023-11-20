75fcff59a272b0f75c5fe9663d9f42599ba7a77d
TRWKTS <T.Pyschopath@gmail.com>
2023-11-20 17:37:48 +0700

    update

--- Models/Account.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_3.Models;
-
-public class Account
-{
-    public string? UAccID { get; set; }
-    public string? UAccUsername { get; set; }
-    public DateTime CreateDateTime { get; set; } = DateTime.Now;
-}
