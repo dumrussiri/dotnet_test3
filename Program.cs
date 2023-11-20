e781c9224eaf900b6e408492267d6e2acfb679ba
MiniProjectWith.Net <dumrussiri6545@gmail.com>
2023-11-20 17:28:59 +0700

    commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using dotnet_3.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>();
 
 var app = builder.Build();
 
