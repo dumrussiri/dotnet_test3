a06c7c179a111cbf50cb056c9f5c9ae276a5f84f
TRWKTS <T.Pyschopath@gmail.com>
2023-11-21 10:29:39 +0700

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
+    "Server=127.0.0.1,1433;Database=Account;User Id=sa;Password=123456Ss;"
 ));
 
 var app = builder.Build();
